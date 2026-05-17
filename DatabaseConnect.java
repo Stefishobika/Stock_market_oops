dc85da2b8bfda65b8cf8d3baf5828c3699da4b97
Stefi Shobika Sukumar <148607546+Stefishobika@users.noreply.github.com>
2026-05-17 19:51:31 +0530

    Add DatabaseConnect class for MySQL connection

---
+++ DatabaseConnect.java
@@ -0,0 +1,31 @@
+package d;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnect {
+
+    private static Connection con;
+
+    public static Connection getConnection() {
+
+        try {
+
+            if(con == null) {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/userdetails",
+                        "root",
+                        "stefi"
+                );
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
