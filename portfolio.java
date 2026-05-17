15ca5f00991adfaf28d4b1f4ff596bee28e5ee41
Stefi Shobika Sukumar <148607546+Stefishobika@users.noreply.github.com>
2026-05-17 19:53:35 +0530

    Create portfolio class extending JFrame

---
+++ portfolio.java
@@ -0,0 +1,14 @@
+package d;
+
+import javax.swing.*;
+
+public class portfolio extends JFrame {
+
+    public portfolio(String username) {
+
+        setTitle("Portfolio");
+        setSize(500,400);
+
+        setVisible(true);
+    }
+}
