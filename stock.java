9da52f0e6d35467e8816ab04145ff1e71589e555
Stefi Shobika Sukumar <148607546+Stefishobika@users.noreply.github.com>
2026-05-17 19:51:49 +0530

    Create stock.java

---
+++ stock.java
@@ -0,0 +1,27 @@
+package d;
+
+public class stock {
+
+    private String symbol;
+    private String name;
+    private double price;
+
+    public stock(String symbol,String name,double price) {
+
+        this.symbol = symbol;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
