a43442dd95152b4d07168fabd1f17a750163dac4
Stefi Shobika Sukumar <148607546+Stefishobika@users.noreply.github.com>
2026-05-17 19:54:05 +0530

    Create transactions class for transaction window

---
+++ transactions.java
@@ -0,0 +1,14 @@
+package d;
+
+import javax.swing.*;
+
+public class transactions extends JFrame {
+
+    public transactions(String username) {
+
+        setTitle("Transactions");
+        setSize(500,400);
+
+        setVisible(true);
+    }
+}
